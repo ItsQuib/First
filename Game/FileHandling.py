1054f2e06232a17f44c50571bf433f515c2ba68a
Quib <jaco@btinternet.com>
2017-04-04 14:10:54 +0100

    Updated FileHandling
    Added more comments, updated variable names.

--- Game/FileHandling.py
+++ Game/FileHandling.py
@@ -4,102 +4,102 @@ from util import clearScreen
 import csv
 
 def ReadOptions():
-    ml = []
-    optl = {}
+    mapList = []
+    optionList = {}
     try:
         with open("options.txt", "r") as a:
-            optl['opt1'] = a.readline()[9:].strip()
-            optl['ng'] = a.readline()[10:].strip()
-            optl['cordx'] = int(a.readline()[16:].strip())
-            optl['cordy'] = int(a.readline()[16:].strip())
-            if(optl['opt1'] == "0"):
+            optionList['newMap'] = a.readline()[9:].strip()
+            optionList['newGame'] = a.readline()[10:].strip()
+            optionList['coordinateX'] = int(a.readline()[16:].strip())
+            optionList['coordinateY'] = int(a.readline()[16:].strip())
+            if(optionList['newMap'] == "0"):
                 with open("map.csv", "r", newline="") as f:
                     if(1 == 1):
                         reader = csv.reader(f)
                         for row, i in zip(reader, range(0,100)):
-                            ml.append([])
+                            mapList.append([])
                             for thing, z in zip(row, range(0,100)):
                                 if(thing != ""):
-                                    ml[i].append([])
-                                    ml[i][z].append(thing[2])
-                                    ml[i][z].append(int(thing[6]))
+                                    mapList[i].append([])
+                                    mapList[i][z].append(thing[2])
+                                    mapList[i][z].append(int(thing[6]))
                                 else:
                                     break
-            return ml, optl
-            if(optl['opt1'] == "1"):
-                optl['cordx'] = randint(0,8)
-                optl['cordy'] = randint(0,8)
-                optl['ng'] = 0
-                optl['opt1'] = 0
+            return mapList, optionList
+            if(optionList['newMap'] == "1"):
+                optionList['coordinateX'] = randint(0,8)
+                optionList['coordinateY'] = randint(0,8)
+                optionList['newGame'] = 0
+                optionList['newMap'] = 0
                 for i in range(0, 9):
-                    ml.append([])
+                    mapList.append([])
                     for o in range(0, 9):
                         r = None
                         r = randint(1,6)
-                        ml[i].append([])
-                        ml[i][o].append("q")
-                        ml[i][o].append(r)
-                ml[optl['cordy']][optl['cordx']][0] = "x"
+                        mapList[i].append([])
+                        mapList[i][o].append("q")
+                        mapList[i][o].append(r)
+                mapList[optionList['coordinateY']][optionList['coordinateX']][0] = "x"
 
                 with open("options.txt", "w") as aa:
                     f.write("New Map: 0\n")
                     f.write("New Game: 0\n")
-                    f.write("Starting Cord X: " + optl['cordx'] + "\n")
-                    f.write("STarting Cord Y: " + optl['cordy'] + "\n")
+                    f.write("Starting Coordinate X: " + optionList['coordinateX'] + "\n")
+                    f.write("Starting Coordinate Y: " + optionList['coordinateY'] + "\n")
                 with open("map.csv", "w", newline="") as f:
                     if(1 == 1):
                         writer = csv.writer(f)
-                        for row, i in zip(ml, range(0,len(ml))):
-                            writer.writerow(ml[i])
-                return ml, optl
+                        for row, i in zip(mapList, range(0,len(mapList))):
+                            writer.writerow(mapList[i])
+                return mapList, optionList
     except FileNotFoundError:
-        optl['cordx'] = randint(0,8)
-        optl['cordy'] = randint(0,8)
-        optl['ng'] = 0
-        optl['opt1'] = 0
+        optionList['coordinateX'] = randint(0,8)
+        optionList['coordinateY'] = randint(0,8)
+        optionList['newGame'] = 0
+        optionList['newMap'] = 0
         for i in range(0, 9):
-            ml.append([])
+            mapList.append([])
             for o in range(0, 9):
                 r = None
                 r = randint(1,6)
-                ml[i].append([])
-                ml[i][o].append("q")
-                ml[i][o].append(r)
-        ml[optl['cordy']][optl['cordx']][0] = "x"
+                mapList[i].append([])
+                mapList[i][o].append("q")
+                mapList[i][o].append(r)
+        mapList[optionList['coordinateY']][optionList['coordinateX']][0] = "x"
 
         with open("options.txt", "w") as f:
             f.write("New Map: 0\n")
             f.write("New Game: 0\n")
-            f.write("Starting Cord X: " + str(optl['cordx']) + "\n")
-            f.write("STarting Cord Y: " + str(optl['cordy']) + "\n")
+            f.write("Starting Coordinate X: " + str(optionList['coordinateX']) + "\n")
+            f.write("Starting Coordinate Y: " + str(optionList['coordinateY']) + "\n")
         with open("map.csv", "w", newline="") as f:
             if(1 == 1):
                 writer = csv.writer(f)
-                for row, i in zip(ml, range(0,len(ml))):
-                    writer.writerow(ml[i])
-        return ml, optl
+                for row, i in zip(mapList, range(0,len(mapList))):
+                    writer.writerow(mapList[i])
+        return mapList, optionList
 
 #############
 #Player File#
 #############
 def ReadPlayer():
-    pll = {}
+    playerList = {}
     try:
         with open("player.txt", "r") as a:
-            pll['player1'] = a.readline()[11:].strip()
-            pll['playername'] = a.readline()[12:].strip()
-            pll['playerhp'] = a.readline()[10:].strip()
-            pll['playerlevel'] = a.readline()[14:].strip()
-            if(pll['player1'] == "1"):
-                user2 = player(pll['playername'], int(pll['playerhp']), 10, 10, int(pll['playerlevel']))
-            if(pll['player1'] == "2"):
-                user2 = player(pll['playername'], int(pll['playerhp']), 5, 10, int(pll['playerlevel']))
-            if(pll['player1'] == "3"):
-                user2 = player(pll['playername'], int(pll['playerhp']), 5, 5, int(pll['playerlevel']))
-            return pll, user2
+            playerList['playerType'] = a.readline()[11:].strip()
+            playerList['playerName'] = a.readline()[12:].strip()
+            playerList['playerHp'] = a.readline()[10:].strip()
+            playerList['playerLevel'] = a.readline()[14:].strip()
+            if(playerList['playerType'] == "1"):
+                playerClass = player(playerList['playerName'], int(playerList['playerHp']), 10, 10, int(playerList['playerLevel']))
+            if(playerList['playerType'] == "2"):
+                playerClass = player(playerList['playerName'], int(playerList['playerHp']), 5, 10, int(playerList['playerLevel']))
+            if(playerList['playerType'] == "3"):
+                playerClass = player(playerList['playerName'], int(playerList['playerHp']), 5, 5, int(playerList['playerLevel']))
+            return playerList, playerClass
     except FileNotFoundError:
-        pll, user2 = Tutorial()
-        return pll, user2
+        playerList, playerClass = Tutorial()
+        return playerList, playerClass
 
 ################
 #Inventory File#
