685948e6135c94b62d720e90c917d854780cd0a3
Quib <jaco@btinternet.com>
2017-04-05 13:40:06 +0100

    Update Mobs.py
    Updated code, added comments and make variable names better.

--- Game/Mobs.py
+++ Game/Mobs.py
@@ -1,52 +1,31 @@
 from random import randint
+#This reads all the mobs, makes a class object for them and puts them in a dictionary
+#The dictionary is then returned.
 def ReadMobs():
-    ml = []
-    md = {}
-    class enemy(object):
-      def __init__(self, name, hp, att, defe, xpaw):
-        self.name = name
-        self.hp = hp
-        self.att = att
-        self.defence = defe
-        self.xpaw = xpaw
-      def info(self):
-        print("--" + self.name + "--")
-        print("Health: " + str(self.hp))
-        print("Attack: " + str(self.att))
-        print("Defence: " + str(self.defence) + "")
-        print("XP Reward: " + str(self.xpaw) + "\n")
-      def retatt(self):
-        return self.att
-      def retdef(self):
-        return self.defence
-      def retname(self):
-        return self.name
-      def rethp(self):
-        return self.hp
-      def retxpaw(self):
-          return self.xpaw
-
+    mobList = []
+    mobDictionary = {}
     with open("Mobs.txt", "r") as f:
         for line in f:
-            ml.append(line.strip())
-    for entry in ml:
-        with open(entry + ".txt", "r") as f:
+            mobList.append(line.strip())
+    for entry in mobList:
+        with open("mobs/" + entry + ".txt", "r") as f:
             filename = entry
             name = f.readline()[6:].strip()
             hp = f.readline()[4:].strip()
             att = f.readline()[8:].strip()
-            defe = f.readline()[9:].strip()
-            xpaw = f.readline()[4:].strip()
-            md[filename] = enemy(name, hp, att, defe, xpaw)
-    return md
+            defence = f.readline()[9:].strip()
+            xpAward = f.readline()[4:].strip()
+            mobDictionary[filename] = enemy(name, hp, att, defence, xpAward)
+    return mobDictionary
 
+#A function to find a random mob and return the mob's class object.
 def RandomMob():
-    l = []
+    mobList = []
     with open("Mobs.txt", "r") as f:
         for line in f:
-            l.append(line.strip())
-    r = randint(0, (len(l) - 1))
-    return l[r]
+            mobList.append(line.strip())
+    randomNo = randint(0, (len(mobList) - 1))
+    return mobList[randomNo]
 
 
 
